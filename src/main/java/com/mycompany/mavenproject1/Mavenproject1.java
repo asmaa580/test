98353a92deb130633a56347083df743b5442ac01
asmaa580 <asmaaadel918273@gmail.com>
2025-10-17 20:28:21 +0300

    BataBase class is written

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -4,13 +4,20 @@
 
 package com.mycompany.mavenproject1;
 
-/**
- *
- * @author USER
- */
+
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EmployeeUser x=new EmployeeUser("456","malak","kfkg@gmail.com"," allam","7101018");
+      EmployeeDataBase y=new EmployeeDataBase();
+      y.insertRecord(x);
+    y.setFilename("Employees.txt");
+    y.readFromFile();
+    y.saveToFile();
+    
+        
+       System.out.println(y.contains("123"));
+      y.deleteRecord("123");
+        System.out.println(y.contains("123"));
     }
 }
